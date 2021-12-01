6a9be9b23bf544bf598efae4e671a2e5522dfaf3
Patryk <patryk_git@outlook.com>
2021-12-01 13:26:15 +0100

    sortowanie przez jedność and small change in bubble sort

--- sortowanie/sortowanie_jednosci.py
+++ sortowanie/sortowanie_jednosci.py
@@ -1,11 +1,12 @@
 from losowe_liczby import losuj
 
 tab = losuj(20)
+
 print(tab)
 
 for x in range(len(tab)-1):                 # tworzymy pętle która będzie działała o jeden raza mniej niż jest długośc tablicy
     for j in range(len(tab)-1):             # bierzemy kolejne elementy tablicy
-        if tab[j] > tab[j+1]:           # jeśli dany element jest większy od następnego to zamień je
+        if int(str(tab[j])[-1]) > int(str(tab[j+1])[-1]):           # jeśli dany element jest większy od następnego to zamień je
             tab[j], tab[j+1] = tab[j+1], tab[j]
     
 
