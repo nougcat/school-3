715f1fb01c52f53ddb785e9b831cc8726f8366d3
Patryk <patryk_git@outlook.com>
2021-12-01 22:42:56 +0100

    naprawiłem sortowanie przez wstawianie

--- sortowanie/wstawianie.py
+++ sortowanie/wstawianie.py
@@ -2,10 +2,19 @@ from losowe_liczby import losuj
 
 tab = losuj(20)
 print(tab)
-for i in range(len(tab)-1):
-    for x in range(i, 0 , -1):
+
+
+for i in range(1, len(tab)):
+    tymczasowe_miejsce = i
+
+    for x in range(i, -1, -1): # szukamy miejsca do którego pasuje nowa zmienna
         if tab[i] < tab[x]:
-            min_loc = x
-    
-    for l in range(min_loc, i):
-        tab[l], tab[l+1] = tab[l+1], tab[l]
+            tymczasowe_miejsce = x
+    tmp_var = tab[i]
+    print(tab)
+    for l in range(tymczasowe_miejsce, i+1):  # wrzucamy zmienną do posortowanego zbioru
+        tmp_var2 = tab[l]
+        tab[l] = tmp_var
+        tmp_var = tmp_var2
+
+print(tab)
