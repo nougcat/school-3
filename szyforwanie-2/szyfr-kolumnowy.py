7c0521aa5a01b8c2b828e361248382c778e6d83d
Patryk <patryk_git@outlook.com>
2021-12-03 09:44:43 +0100

    szyfr kolumnowy - dokoncz

---
+++ szyforwanie-2/szyfr-kolumnowy.py
@@ -0,0 +1,9 @@
+kl = 1
+szyfrogram = ''
+
+with open('plik.txt', 'r') as f:
+    for linia in f:
+        for i in range(len(linia)):
+            for j in range():
+                szyfrogram +=
+        print(szyfrogram)
