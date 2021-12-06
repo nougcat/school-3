f67a57fd39f81676721e9be793113f10dd6addac
Patryk <patryk_git@outlook.com>
2021-12-06 07:52:41 +0100

    poprawione komentarze i szyfr kolumnowy jest w funkcji

--- szyfrowanie-2/szyfr-jak-excel.py
+++ szyfrowanie-2/szyfr-jak-excel.py
@@ -1,17 +1,19 @@
+# idk to jest dziwny szyfr
+
 slowo = str(input('podaj slowo: '))
 ilosc_kolumn = int(input('podaj liczbe: '))
 
-a = ilosc_kolumn - len(slowo)%ilosc_kolumn
-if a == ilosc_kolumn: a = 0
-for x in range(a): slowo += 'x'
+while (len(slowo)%ilosc_kolumn != 0): slowo += 'x'  # dopełniamy tekst do póki nie jest wielokrotnością ilości kolumn
 
-
-print(slowo)
 wynik = ''
 
+# ogólnie to ta pętla wypisuje co któryś element stringa, a potem co któryś +1
+# dla hasła DUPA i klucza 2
+# D P
+# U A
+# == DPUA
 for i in range(ilosc_kolumn):
     for x in range(int(len(slowo)/ilosc_kolumn)):
-        print(x * ilosc_kolumn)
         wynik += slowo[i + x * ilosc_kolumn]
 
 print(wynik)
