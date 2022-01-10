50d0f683f06e54549f39ef9bc34e48054bce9254
Patryk <patryk_git@outlook.com>
2022-01-10 07:53:21 +0100

    dokończ w domu

--- sito.py
+++ sito.py
@@ -1,2 +1,13 @@
 # sito eratostenesa
 
+num = int(input('podaj liczbe: '))
+x = 2
+tab = [False] + [True for i in range(x, num+1)]
+
+while x * x <= num:
+    for i in range(0, num-x, x): 
+        tab[x * x - 2 + i] = False 
+    
+        x += 1
+
+print(tab)
