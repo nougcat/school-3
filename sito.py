2927efdd65817b5c6b6ba9b1ee12450ae713342f
Patryk <patryk_git@outlook.com>
2022-01-13 11:33:05 +0100

    to do @ home

--- sito.py
+++ sito.py
@@ -4,10 +4,12 @@ num = int(input('podaj liczbe: '))
 x = 2
 tab = [False] + [True for i in range(x, num+1)]
 
-while x * x <= num:
-    for i in range(0, num-x, x): 
-        tab[x * x - 2 + i] = False 
-    
-        x += 1
+while x * x < num:
+    for i in range(x, i * x, 2):
+        tab[i] = False
 
-print(tab)
+        x += 2
+
+
+for x in range(len(tab)):
+    print(x, tab[x])
