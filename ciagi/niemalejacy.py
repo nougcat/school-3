e4bb83cd92abff5dfbd3f68b760b95fb9eb28b4d
Patryk <patryk_git@outlook.com>
2022-01-17 07:54:40 +0100

    wypisywanie najdłuższego ciągu

--- ciagi/niemalejacy.py
+++ ciagi/niemalejacy.py
@@ -8,18 +8,20 @@ def dlugosc_ciagu_ros(ciag):
     aktualny_ciag = 1
     for x in range(len(ciag)-1):
         if ciag[x] <= ciag[x+1]:
-            print(ciag[x], ciag[x-1])
             aktualny_ciag += 1
             if max_ciag < aktualny_ciag:
                 max_ciag = aktualny_ciag
+                indeks = x
         else:
             aktualny_ciag = 1
-    return max_ciag
+    return max_ciag, indeks+1
+if __name__=='__main__':
+    dlg_ciagu = int(input('podaj dlugosc ciagu: '))
+    ciag = losuj(dlg_ciagu)
 
-dlg_ciagu = int(input('podaj dlugosc ciagu: '))
-ciag = losuj(dlg_ciagu)
+    print(ciag)
 
-print(ciag)
-
-odp = dlugosc_ciagu_ros(ciag)
-print(odp)
+    odp, indeks = dlugosc_ciagu_ros(ciag)
+    print(odp)
+    print(indeks)
+    print(ciag[indeks-odp+1:indeks+1])
