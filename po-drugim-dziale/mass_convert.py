8ee29c3aad76277ab0173b5ceb6ee3377b2fd92e
Patryk <patryk_git@outlook.com>
2022-03-03 10:38:49 +0100

    making it more funcion based programming

--- po-drugim-dziale/mass_convert.py
+++ po-drugim-dziale/mass_convert.py
@@ -1,18 +1,23 @@
 suma = 0
 # otwieramy plik
-max = [0,0]
-min = [-1]
+max = [0, 0]
+min = [0, 999]
 
 def min_and_max(dec_val_of_line, system):
-    if min[0] == -1:
-        min[0] = system
-        min.append(dec_val_of_line)
-    
     if dec_val_of_line > max[1]:
         max[0], max[1] = system, dec_val_of_line
-    elif len(min) == 2 and dec_val_of_line < min[1]:
+    elif dec_val_of_line < min[1]:
         min[0], min[1] = system, dec_val_of_line
-    
+
+def to_dec(system, liczba):
+    results = 0
+    y = 0
+    while y < len(liczba):
+        dec_val = int(liczba) * (system ** (len(liczba)) - y - 1)
+        results += dec_val
+        y += 1
+    print(results)
+    return results
     
 
 with open('wuz2-zad1-liczby.txt', 'r') as f:
@@ -22,17 +27,11 @@ with open('wuz2-zad1-liczby.txt', 'r') as f:
         # zapisujemy w jakim systemie jest liczba
         system = int(linia[0])
         liczba = linia[1]
-        y = 0
-        while y < len(linia[1]):
-            print(system ** (len(linia[1])-y))
-            print(f'dupa {y}')
-            print(liczba[y])
-            
-            dec_val_of_line = int(liczba[y]) * (system ** (len(linia[1]) - y - 1))
-            suma += dec_val_of_line
-            y += 1
 
-            min_and_max(dec_val_of_line, system)
+        dec_val_of_line = to_dec(system, liczba) 
+        suma += dec_val_of_line
+
+        min_and_max(dec_val_of_line, system)
     
 print(f'suma - {suma}')
 print(min)
