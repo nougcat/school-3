b23cbc8be89f4a1c85398928b88a8c0dd5c72bf5
Patryk <patryk_git@outlook.com>
2022-04-03 17:44:18 +0200

    reszta z zakupów

--- rekurencja/reszta_zachlanna.py
+++ rekurencja/reszta_zachlanna.py
@@ -1,15 +1,30 @@
 def dawaj_reszte(kwota):
+    ini_kwota = kwota
     reszta = []
     for x in nominaly:
         if kwota % x != kwota:
-            reszta += [x * int((kwota/x))]
+            if ini_kwota == kwota:
+                ile_razy = int(kwota/x)
+            reszta += [x]
             kwota = kwota % x
-    return reszta
+    return reszta, ile_razy
 
-nominaly = [500,200,100,50,20,10,5,2,1, 0.5 , 0.2, 0.1, 0.05, 0.02, 0.01]
 
-a = int(input('podaj kwote: '))
+if __name__=='__main__':
+    nominaly = [500,200,100,50,20,10,5,2,1, 0.5 , 0.2, 0.1, 0.05, 0.02, 0.01]
 
-reszta = dawaj_reszte(a)
+    a = float(input('podaj kwote: '))
 
-print(reszta)
+    reszta, ile_razy = dawaj_reszte(a)
+
+    for x in reszta:
+        if x == reszta[0]:
+            if x >=1:
+                print(f'{ile_razy} * {x}zł')
+            else:
+                print(f'{ile_razy} * {int(x * 100)}gr')
+
+        elif x>=1:
+            print(f'1 * {x}zł')
+        else:
+            print(f'1 * {int(x * 100)}gr')
