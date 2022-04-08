997551493498aed0911a3e607d25f433ea0680dc
Patryk <patryk_git@outlook.com>
2022-04-08 09:44:21 +0200

    dokoncz w domu

--- algorytmy_zachlanne/podciag_max.py
+++ algorytmy_zachlanne/podciag_max.py
@@ -1,5 +1,15 @@
-from random import randint, seed 
+from random import randint, seed
+from webbrowser import Konqueror 
 
+def ruch(pole[], koordynaty):
+    if koordynaty[0] == pole[-1][koordynaty[1]]:
+        a = 0
+    else:
+        a = pole[koordynaty+1]
+    if koordynaty[1] == pole[koordynaty[1]][-1]:
+        b = 0
+    else:
+        b = pole[koordynaty]
 
 wymiary=6
 pole = []
@@ -7,3 +17,7 @@ for x in range(wymiary):
     pole += [[randint(0,9) for x in range(wymiary)]]
 
 print(pole)
+
+koordynaty = [0,0]
+
+ruch(pole, koordynaty)
