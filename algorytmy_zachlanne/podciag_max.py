a8f88ba05496192821d277b1395b0f8ef49b6cf2
Patryk <patryk_git@outlook.com>
2022-04-11 07:40:41 +0200

    naj podciąg - metoda zachłanna

--- algorytmy_zachlanne/podciag_max.py
+++ algorytmy_zachlanne/podciag_max.py
@@ -1,23 +1,37 @@
-from random import randint, seed
-from webbrowser import Konqueror 
+from random import randint
 
-def ruch(pole[], koordynaty):
-    if koordynaty[0] == pole[-1][koordynaty[1]]:
+def ruch(pole, koordynaty):
+    print(pole[koordynaty[0]][koordynaty[1]])
+
+    if koordynaty[0] == wymiary-1:
         a = 0
     else:
-        a = pole[koordynaty+1]
-    if koordynaty[1] == pole[koordynaty[1]][-1]:
+        a = pole[koordynaty[0]+1][koordynaty[1]]
+    if koordynaty[1] == wymiary-1:
         b = 0
     else:
-        b = pole[koordynaty]
+        b = pole[koordynaty[0]][koordynaty[1]+1]
+    
+    if a >= b:
+        koordynaty[0] += 1
+        return a
+    else:
+        koordynaty[1] += 1
+        return b
+
 
 wymiary=6
 pole = []
 for x in range(wymiary):
-    pole += [[randint(0,9) for x in range(wymiary)]]
-
+    pole += [[randint(1,9) for x in range(wymiary)]]
+suma = 0
 print(pole)
 
 koordynaty = [0,0]
 
-ruch(pole, koordynaty)
+while koordynaty != [wymiary-1,wymiary-1]:
+    suma += ruch(pole, koordynaty)
+
+print(pole[koordynaty[0]][koordynaty[1]])
+
+print(suma)
