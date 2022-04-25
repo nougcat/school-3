f51d2ad3a5bf259fa90a128dffb93484b9cf2828
Patryk <patryk_git@outlook.com>
2022-04-25 07:55:15 +0200

    wydawanie monet (niedokonczone)

---
+++ dynamiczne/wydawanie_monet.py
@@ -0,0 +1,11 @@
+def wydaj_reszte():
+    pass
+
+def max_kwota():
+    pass
+
+nominaly = [500,200,100,50,20,10,5,2,1, 0.5 , 0.2, 0.1, 0.05, 0.02, 0.01]
+
+kwota = float(input('podaj kwote: '))
+
+max_kwota()
