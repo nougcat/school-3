72ade5ebfcbfeb4a2ad6644c8cf382a75dde1b82
Patryk <patryk_git@outlook.com>
2022-04-28 10:40:43 +0200

    dokończ w domu (wydawanie monet met. dynamiczna)

--- dynamiczne/wydawanie_monet.py
+++ dynamiczne/wydawanie_monet.py
@@ -1,11 +1,24 @@
-def wydaj_reszte():
-    pass
+def wydaj_reszte(kwota, start):
+    kwota_startowa = kwota
+    reszta = [0 for x in (nominaly-start)]
+    for i, x in enumerate(nominaly, start):
+        if kwota % x != kwota:
+            if kwota_startowa == kwota:
+                reszta[i] = int(kwota/x)
+            kwota = kwota % x
+    return reszta
 
-def max_kwota():
-    pass
+def max_kwota(kwota):
+    for i, x in enumerate(nominaly):
+        if kwota >= x:
+            return i
 
 nominaly = [500,200,100,50,20,10,5,2,1, 0.5 , 0.2, 0.1, 0.05, 0.02, 0.01]
 
-kwota = float(input('podaj kwote: '))
+if __name__=='__main__':
+    kwota = float(input('podaj kwote: '))
 
-max_kwota()
+    start = max_kwota(kwota)
+
+    for x in enumerate(nominaly, start):
+        print(wydaj_reszte(kwota, start))
