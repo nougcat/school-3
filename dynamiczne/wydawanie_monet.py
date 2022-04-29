147c01f29bbc62942491e2a7886a4d5d8fb848a3
Patryk <patryk_git@outlook.com>
2022-04-29 09:46:04 +0200

    wydawanie monet met. dynamiczna

--- dynamiczne/wydawanie_monet.py
+++ dynamiczne/wydawanie_monet.py
@@ -1,24 +1,43 @@
 def wydaj_reszte(kwota, start):
-    kwota_startowa = kwota
-    reszta = [0 for x in (nominaly-start)]
-    for i, x in enumerate(nominaly, start):
-        if kwota % x != kwota:
-            if kwota_startowa == kwota:
-                reszta[i] = int(kwota/x)
-            kwota = kwota % x
+    # tworzymy pustą tablicę
+    reszta = [0 for x in (nominaly)]
+
+    for i in range(start, len(nominaly)):
+        if kwota % nominaly[i] != kwota:
+            reszta[i] = int(kwota/nominaly[i])
+            kwota = kwota % nominaly[i]
+    
     return reszta
 
 def max_kwota(kwota):
+    # usunięcie największych nominalów jeśli nie jest potrzebne
     for i, x in enumerate(nominaly):
         if kwota >= x:
             return i
 
-nominaly = [500,200,100,50,20,10,5,2,1, 0.5 , 0.2, 0.1, 0.05, 0.02, 0.01]
+def min_nominalow(reszty):
+    # najmiejsza ilość nominałów do wydania reszty
+    min_ilosc_nominalow = not None
+    for x in reszty:
+        aktualna_ilosc_nominalow = 0
+        for i in x:
+            aktualna_ilosc_nominalow += i
+        if min_ilosc_nominalow > aktualna_ilosc_nominalow: min_ilosc_nominalow = aktualna_ilosc_nominalow
+        print(aktualna_ilosc_nominalow)
+        print(min_ilosc_nominalow)
+    return min_ilosc_nominalow
+
+nominaly = [500, 200, 100, 50, 20, 10, 5, 2, 1, 0.5 , 0.2, 0.1, 0.05, 0.02, 0.01]
 
 if __name__=='__main__':
     kwota = float(input('podaj kwote: '))
 
     start = max_kwota(kwota)
 
-    for x in enumerate(nominaly, start):
-        print(wydaj_reszte(kwota, start))
+    reszty = []
+    start -= 1
+    for x in range(start, len(nominaly)-1):
+        start += 1
+        reszty.append(wydaj_reszte(kwota, start))
+
+    print(min_nominalow(reszty))
