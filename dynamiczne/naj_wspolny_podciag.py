1b53fd446878e8ce12adfb7bfc357f886b1d80f0
Patryk <patryk_git@outlook.com>
2022-05-24 08:50:20 +0200

    praca domowa + komentarze

--- dynamiczne/naj_wspolny_podciag.py
+++ dynamiczne/naj_wspolny_podciag.py
@@ -5,10 +5,11 @@ podciag = ''
 
 zaczynamy_od = 0
 for x in pierwszy_wyraz:
+    
     for i in range(zaczynamy_od, len(drugi_wyraz)):
-        if x == drugi_wyraz[i]:
-            podciag += x
-            zaczynamy_od = i + 1
+        if x == drugi_wyraz[i]:     # jeśli elementy są takie same to:
+            podciag += x            # dodajemy element do podciągu
+            zaczynamy_od = i + 1    # zaczynamy od następnego elementu
             break
         
 print(f'{podciag} - {len(podciag)}')
