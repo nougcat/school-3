37b08266bb5bccaea598429e7fcceae33bb36df0
Patryk <patryk_git@outlook.com>
2022-05-27 07:29:21 +0200

    funkcja teraz dziala jako funkcja plus zrobienie funkcji tak aby dzialala na szkopule

--- dynamiczne/naj_wspolny_podciag.py
+++ dynamiczne/naj_wspolny_podciag.py
@@ -1,15 +1,23 @@
-pierwszy_wyraz = input('podaj pierwszy wyraz: ')
-drugi_wyraz = input('podaj drugi wyraz: ')
+def najdluzszy_wspolny_podciag(pierwszy_wyraz, drugi_wyraz):
+    podciag = ''
+    zaczynamy_od = 0
 
-podciag = ''
-
-zaczynamy_od = 0
-for x in pierwszy_wyraz:
+    for x in pierwszy_wyraz:
     
-    for i in range(zaczynamy_od, len(drugi_wyraz)):
-        if x == drugi_wyraz[i]:     # jeśli elementy są takie same to:
-            podciag += x            # dodajemy element do podciągu
-            zaczynamy_od = i + 1    # zaczynamy od następnego elementu
-            break
-        
-print(f'{podciag} - {len(podciag)}')
+        for i in range(zaczynamy_od, len(drugi_wyraz)):
+            if x == drugi_wyraz[i]:     # jeśli elementy są takie same to:
+                podciag += x            # dodajemy element do podciągu
+                zaczynamy_od = i + 1    # zaczynamy od następnego elementu
+                break
+    
+    return podciag
+
+
+pierwszy_wyraz = input()    # podaj pierwszy wyraz
+drugi_wyraz = input()       # podaj drugi wyraz
+
+podciag = najdluzszy_wspolny_podciag(pierwszy_wyraz, drugi_wyraz)
+
+
+print(len(podciag))
+print(podciag)
